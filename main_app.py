4fb4fe417742f67cab1420b01dc1eac9cfd8b7da
Mukesh Tiwari <mukeshtiwari1987@gmail.com>
2019-06-20 16:19:31 +0530

    Added support for searching network logs

--- main_app.py
+++ main_app.py
@@ -6,6 +6,7 @@ from setup import UPLOAD_FOLDER, DOWNLOAD_FOLDER, remove_and_create_download_fol
 from command_parser import command_parser_main
 from vid_parser import vid_parser_main
 from console_parser import console_parser_main
+from networklog_parser import network_parser_main
 
 app = Flask(__name__, static_url_path="/static")
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -110,6 +111,31 @@ def aut_console_result():
             remove_and_create_download_folder()
             return render_template("autconsoleresult.html", output_list=results)
     return render_template('upload_autvid.html')
+
+
+@app.route('/autnetwork')
+def aut_network():
+    return render_template('upload_autnetwork.html')
+
+
+@app.route('/networkstatusaut', methods=['POST'])
+def aut_network_result():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            print('No file attached in request')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            print('No file selected')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            search_text = request.form['search_text']
+            results = network_parser_main(filename, search_text)
+            remove_and_create_download_folder()
+            return render_template("autnetworkresult.html", output_list=results)
+    return render_template('upload_autnetwork.html')
 
 
 if __name__ == '__main__':
